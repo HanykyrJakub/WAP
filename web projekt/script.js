9e4865ec259cfc7bf3508bdb33d86fa018074edc
HanykyrJakub <90202111+HanykyrJakub@users.noreply.github.com>
2021-12-07 16:16:18 +0100

    Add files via upload

--- web projekt/script.js
+++ web projekt/script.js
@@ -1,7 +1,22 @@
-let form = document.getElementById("form");
- form.onsubmit = (e) => {
-    e.preventDefault();
-    let age = document.getElementById("age").value
-    let books = document.getElementById("books").value
-    console.log(age, books);
-}
+		var list1 = [];
+		var list2 = [];
+		
+        var n = 1;
+		var x = 0;
+
+		function AddRow(){
+
+			var AddRown = document.getElementById('show');
+			var NewRow = AddRown.insertRow(n);
+
+			list1[x] = document.getElementById("age").value;
+			list2[x] = document.getElementById("books").value;
+        
+            var cel1 = NewRow.insertCell(0);
+			var cel2 = NewRow.insertCell(1);
+
+			cel1.innerHTML = list1[x];
+			cel2.innerHTML = list2[x];
+			n++;
+			x++;
+		}
