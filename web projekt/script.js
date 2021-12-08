99f98cc4e58ead4c99618095217bae81527b144f
HanykyrJakub <90202111+HanykyrJakub@users.noreply.github.com>
2021-12-08 16:00:46 +0100

    Add files via upload

--- web projekt/script.js
+++ web projekt/script.js
@@ -1,22 +1,32 @@
-		var list1 = [];
-		var list2 = [];
-		
-        var n = 1;
-		var x = 0;
-
-		function AddRow(){
-
-			var AddRown = document.getElementById('show');
-			var NewRow = AddRown.insertRow(n);
-
-			list1[x] = document.getElementById("age").value;
-			list2[x] = document.getElementById("books").value;
-        
-            var cel1 = NewRow.insertCell(0);
-			var cel2 = NewRow.insertCell(1);
-
-			cel1.innerHTML = list1[x];
-			cel2.innerHTML = list2[x];
-			n++;
-			x++;
-		}
+let data = {};
+let table = document.getElementById("table");
+let form = document.getElementById("form");
+form.onsubmit = (e) => {
+	e.preventDefault();
+	let age = parseInt(document.getElementById("age").value);
+	let books = parseInt(document.getElementById("books").value);
+	if (data[age] == undefined) {
+		data[age] = { age: age, books: books, count: 1};
+	} else {
+		data[age].books += books;
+		data[age].count++;
+	}
+	table.innerHTML = 
+		"<tr><th>věk</th><th>lidí</th><th>knih</th><th>knih na člověka</th></tr>";
+		for (let age in data) {
+			let tr = document.createElement("tr");
+			let td = document.createElement("td");
+			td.innerText = age;
+			tr.appendChild(td);
+			td = td.cloneNode();
+			td.innerText = data[age].count;
+			tr.appendChild(td);
+			td = td.cloneNode();
+			td.innerText = data[age].books;
+			tr.appendChild(td);
+			td = td.cloneNode();
+			td.innerText = Math.round((data[age].books / data[age].count) * 10) / 10;
+			tr.appendChild(td);
+			table.appendChild(tr);
+	}
+}
