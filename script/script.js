6d8fd0a9d9f4dc7f4d7afb157002d9721a1e8b36
HanykyrJakub <90202111+HanykyrJakub@users.noreply.github.com>
2021-12-31 15:47:22 +0100

    Delete script directory

--- script/script.js
+++
@@ -1,8 +0,0 @@
-function myFunction() {
-    var link = document.getElementById("um");
-    var segments = link.href.split('/');
-    var currentStyle = segments[segments.length - 1];
-    var style =  (currentStyle == 'styl_1.scss') ? 'styl_2.scss' 
-                                                     : 'styl_1.scss';
-    document.getElementById("um").href = style + ".scss"
-  }
