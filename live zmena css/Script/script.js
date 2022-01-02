55a6ae40abd395b80c468bfa8c34a38f915339b9
HanykyrJakub <90202111+HanykyrJakub@users.noreply.github.com>
2022-01-02 17:13:50 +0100

    readme.md
    LIVE CHANGE CSS

---
+++ live zmena css/Script/script.js
@@ -0,0 +1,25 @@
+let before = 1;
+let potee = 2;
+let poteee = 3;
+let poteeee = 4;
+function before(){
+    document.getElementById('styl_1')
+    .src ="styles/styl"; 
+}
+function potee(){
+    document.getElementById('styl_2')
+    .src = "styles/mod_2";
+}
+function poteee(){
+    document.getElementById('styl_3')
+    .src="styles/mod_3";
+}
+function poteeee(){
+    document.getElementById('styl_4')
+    .src="styles/mod_4";
+}
+for (before = 1; before<after_2;before++){
+    if (before != potee){
+        console.log(0);
+    }
+}
