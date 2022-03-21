62054c07951dbb691f3fcbd830b83548974c2d6a
HanykyrJakub <90202111+HanykyrJakub@users.noreply.github.com>
2022-03-21 13:29:05 +0100

    Add files via upload

--- AUKCE/JS/data.js
+++ AUKCE/JS/data.js
@@ -3,26 +3,22 @@ const data ={
         name: "rychlost větru",
         desc: "abstrakce větru v uzav. prostoru",
         author: "Jakub Hanykyr",
-        image:,
     },
 
     "p002":{
         name: "Vesmír",
         desc: "socha představující nekonečnost",
         author: "Jiři Hanyk",
-        image:,
     },
     "p003":{
         name: "Slunce",
         desc: "Alegorie na reaqlitu",
         author: "Jonas Safranek",
-        image:,
     },
 
     "p004":{
         name: "Hněv",
         desc: "Pocit",
         author: "Jonas Safranek",
-        image:,
     }
 }
--- AUKCE/JS/script.js
+++ AUKCE/JS/script.js
@@ -3,7 +3,6 @@ for(let index in data){
     let item = data[index];
     let ele =document.createElement("div");
     ele.innerHTML = `
-    <figure><img src = "./data/${item.image}
     <h2>${item.name}</h2>
     <p>${item.desc}</p>
     <p>${item.author}</p>`;
